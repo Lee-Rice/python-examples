7fe9e54b250cb13b190fa0822e7b3e60aea32bd3
Lee Rice <132123650+Lee-Rice@users.noreply.github.com>
2024-12-31 17:49:02 -0500

    Created initial examples

---
+++ sources/example_data.py
@@ -0,0 +1,25 @@
+'''Data used for examples.'''
+
+name_list: list = [
+    "Ed",
+    "Joe",
+    "Kim",
+    "Alvin",
+    "Ed",
+    "Kim",
+]
+animals = {
+    "Bill": "cat",
+    "Sam": "Dog",
+    "Tina": {
+        "animal": "tiger",
+        "age": 4,
+        "siblings": ["Tom", "Lauren", "Heather", "Alan"],
+    },
+}
+plants = {
+    "Herb": {"Type": "Thyme", "Age": 2},
+    "Sunny": {"Type": "Sunflower", "Age": 1},
+    "Alvin": {"Type": "Aloe Vera", "Age": 2},
+    "Dorothy": {"Type": "Rose", "Age": 1},
+}
